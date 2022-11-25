ef831128b4a6acd51d5671e8468cae336a3f95ac
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-11-26 02:59:30 +0600

    Problem 116A

---
+++ problem116A/Program.cs
@@ -0,0 +1,14 @@
+﻿int n, a, b, sum = 0, k = 0;
+n = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 0; i < n; i++)
+{
+    string[] data = Console.ReadLine().Split();
+    a = Convert.ToInt32(data[0]);
+    b = Convert.ToInt32(data[1]);
+
+    sum = (a + k) - b;
+    if (sum < 0) k = 0;
+    else k = sum;
+}
+Console.WriteLine(sum);
