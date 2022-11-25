0db671676c84d73843dbb14a17db7e380d7dd45a
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-11-26 03:00:52 +0600

    Problem 579A

---
+++ problem579A/Program.cs
@@ -0,0 +1,18 @@
+﻿int nob, x;
+
+nob = Convert.ToInt32(Console.ReadLine());
+while (true)
+{
+    x = 0;
+    while (nob > 1)
+    {
+        if (nob % 2 == 0)
+            nob = nob / 2;
+        else
+        {
+            nob = nob - 1; x++;
+        }
+    }
+    break;
+}
+Console.WriteLine(  x + 1);
