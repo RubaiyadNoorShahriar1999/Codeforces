464d01c77a9d0d19825e94cbdd159fcadddcb5e2
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-11-26 02:59:01 +0600

    Problem 112A

---
+++ problem112A/Program.cs
@@ -0,0 +1,35 @@
+﻿string string1, string2;
+
+string1 = Console.ReadLine().ToLower();
+string2 = Console.ReadLine().ToLower();
+
+int sum1 = string1.Sum(b => b);
+int sum2 = string2.Sum(c => c);
+
+if (string1 == string2)
+{
+    Console.WriteLine(0);
+}
+else
+{
+    for (int i = 0; i < string1.Length; i++)
+    {
+        if (string1[i] < string2[i])
+        {
+            Console.WriteLine(-1);
+            break;
+        }
+
+        if (string1[i] > string2[i])
+        {
+            Console.WriteLine(1);
+            break;
+        }
+
+    }
+}
+
+
+
+
+
