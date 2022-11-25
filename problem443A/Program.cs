274d5ecfb124848e2bb3284640793704157019ce
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-11-26 03:00:21 +0600

    Problem 443A

---
+++ problem443A/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+List<char> nums = new List<char>(); 
+
+for (int i = 0; i < 1000; i++)
+{
+    char input = (char)Console.Read(); 
+    if (input == '}')
+    {
+        break;
+    }
+    else
+    {
+        nums.Add(input);
+    }
+}
+int counter = 0;
+List<char> myList = nums.Distinct().ToList();
+
+for (int i = 0; i < myList.Count; i++)
+{
+    if (myList[i] != '{' && myList[i] != '}' && myList[i] != ' ' && myList[i] != ',')
+    {
+        counter++;
+    }
+}
+Console.Write(counter);
