3427d965a1d5ba88121bd57cfc6e57771646364f
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-11-26 05:24:29 +0600

    Problem 1335A

---
+++ problem1335A/Program.cs
@@ -0,0 +1,18 @@
+﻿int t;
+t = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 0; i < t; i++)
+{
+    int n;
+    n = Convert.ToInt32(Console.ReadLine());
+
+    if( n % 2 == 0)
+    {
+        Console.WriteLine((n / 2) - 1);
+    }
+    else
+    {
+        Console.WriteLine(n/2);
+    }
+}
+
