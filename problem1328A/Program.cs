6114edb697de781578002faa40a6247ffd0bf7b0
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-11-26 05:20:23 +0600

    Problem 1328A

---
+++ problem1328A/Program.cs
@@ -0,0 +1,21 @@
+﻿int test_cases, i;
+test_cases = Convert.ToInt32(Console.ReadLine());
+
+int var1, var2;
+for(i = 0; i < test_cases; i++)
+{
+    string[] data = Console.ReadLine().Split();
+    var1 = Convert.ToInt32(data[0]);
+    var2 = Convert.ToInt32(data[1]);
+
+        if (var1 % var2 == 0)
+        {
+            Console.WriteLine(0);
+        }
+        else
+        {
+            int w = var1 % var2;
+            Console.WriteLine( var2 - w + "\t");
+        }
+
+}
