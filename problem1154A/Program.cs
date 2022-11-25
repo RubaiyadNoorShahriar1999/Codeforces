1a5cd5336fca4bb4130d949ce793279833c4dffb
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-11-26 03:02:59 +0600

    Problem 1154A

---
+++ problem1154A/Program.cs
@@ -0,0 +1,13 @@
+﻿List<int> encrypted = new List<int>();
+
+string input = Console.ReadLine();
+string[] data = input.Split(" "); 
+
+foreach(var d in data)
+{
+    encrypted.Add(Convert.ToInt32(d)); 
+}
+
+encrypted.Sort();
+for (int i = 0; i < 3; i++)
+    Console.WriteLine(encrypted[3] - encrypted[i] + " "); 
