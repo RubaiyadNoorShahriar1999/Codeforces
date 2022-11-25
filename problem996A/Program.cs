9b296c71d4af68a9d9e74524d77532b5c94e4898
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-11-26 03:02:25 +0600

    Problem 996A

---
+++ problem996A/Program.cs
@@ -0,0 +1,37 @@
+﻿int count = 0;
+
+int amount = Convert.ToInt32(Console.ReadLine());
+
+while (amount <= 1000000000)
+{
+    if (amount >= 100)
+    {
+        count++; 
+        amount = amount - 100; 
+    }
+    else if (amount >= 20) 
+    { 
+        count++; 
+        amount = amount - 20;
+    }
+    else if (amount >= 10) 
+    { 
+        count++;
+        amount = amount - 10;
+    }
+    else if (amount >= 5) 
+    { 
+        count++; 
+        amount = amount - 5; 
+    }
+    else if(amount >= 1)
+    { 
+        count++; 
+        amount = amount - 1;
+    }
+    else
+    {
+        break;
+    }
+}
+Console.WriteLine(count);
