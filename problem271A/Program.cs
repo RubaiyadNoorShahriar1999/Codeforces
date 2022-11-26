c975c49de1f233de42b42bced32b1387d7f51192
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-11-26 22:54:35 +0600

    Problem 271A

---
+++ problem271A/Program.cs
@@ -0,0 +1,16 @@
+﻿int year;
+year = Convert.ToInt32(Console.ReadLine());
+
+while (true)
+{
+    year += 1;
+    int first_digit = year / 1000;
+    int second_digit = year / 100 % 10;
+    int third_digit =  year / 10 % 10;
+    int last_digit = year % 10;
+
+    if (first_digit != second_digit && first_digit != third_digit && first_digit != last_digit && second_digit != third_digit && second_digit != last_digit && third_digit != last_digit)
+        break;
+
+}
+Console.WriteLine(year);
