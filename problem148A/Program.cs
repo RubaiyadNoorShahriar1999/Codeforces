1789a5cee1f9662e9bf214106436fdba8ccedf55
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-11-26 22:53:36 +0600

    Problem 148A

---
+++ problem148A/Program.cs
@@ -0,0 +1,10 @@
+﻿int k, l, m, n, d;
+k = Convert.ToInt32(Console.ReadLine());
+l = Convert.ToInt32(Console.ReadLine());
+m = Convert.ToInt32(Console.ReadLine());
+n = Convert.ToInt32(Console.ReadLine());
+d = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+for (int i = 1; i <= d; i++) { if (i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0) count++; }
+Console.WriteLine(count);
