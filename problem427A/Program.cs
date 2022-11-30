cea651a6c1851c7b4bb6dc78744aeed080bfde8f
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-11-30 22:53:41 +0600

    Problem 427A

---
+++ problem427A/Program.cs
@@ -0,0 +1,29 @@
+﻿int n;
+n = Convert.ToInt32(Console.ReadLine());
+int curoff = 0;
+int untreated = 0;
+string[] data;
+data = Console.ReadLine().Split();
+
+
+for (int i = 0; i < n; i++)
+{
+    int x = Convert.ToInt32(data[i]);
+    if (x > 0)
+    {
+        curoff += x;
+
+    }
+    else
+    {
+        if (curoff < 1)
+        {
+            ++untreated;
+        }
+        else
+        {
+            --curoff;
+        }
+    }
+}
+Console.WriteLine(untreated);
