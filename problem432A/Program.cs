bb286c27a08f16b980dfbf5ea7924169fc1f1d51
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-11-30 22:53:04 +0600

    Problem 432A

---
+++ problem432A/Program.cs
@@ -0,0 +1,36 @@
+﻿int n, k , count = 0;
+
+string[] data = new string[2];
+data = Console.ReadLine().Split();
+n = Convert.ToInt32(data[0]);
+k = Convert.ToInt32(data[1]);
+
+int[] a = new int[n];
+
+string[] data2 = new string[n];
+data2 = Console.ReadLine().Split();
+
+for (int i = 0; i < n; i++)
+{
+    a[i] = Convert.ToInt32(data2[i]);
+}
+
+for(int i = 0; i < n; i++)
+{
+    if (a[i] < 5)
+    {
+        if(5 - a[i] >= k)
+        {
+            count++;
+        }
+    }
+}
+
+if(count >= 3)
+{
+    Console.WriteLine(count / 3);
+}
+else
+{
+    Console.WriteLine(0);
+}
