1317d062938d1547fd9bd646a0cb707723eea00f
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-11-30 22:54:21 +0600

    Problem 339A

---
+++ problem339A/Program.cs
@@ -0,0 +1,28 @@
+﻿int count = 0;
+List<int> arr = new List<int>(100);
+
+string[] data;
+data = Console.ReadLine().Split("+");
+
+for (int i = 0; i < data.Length; i++)
+{
+    if (Convert.ToChar(data[i]) == '+')
+    {
+        continue;
+    }
+    else
+    { 
+        arr.Add(Convert.ToInt32(data[i]));
+    }
+}
+
+arr.Sort();
+for (int i = 0; i < arr.Count; i++)
+{
+    Console.Write(Convert.ToInt32(arr[i]));
+    if (i == arr.Count - 1)
+    {
+        break;
+    }
+    Console.Write("+");
+}
