b58f060391829ec4a5ac661481d3f75e1fd3c390
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-11-30 22:51:25 +0600

    Problem 266A

---
+++ problem266A/Program.cs
@@ -0,0 +1,14 @@
+﻿string b;
+int a, c = 0;
+
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToString(Console.ReadLine()+ " ");
+
+for (int i = 0; i < a; i++)
+{
+    if (b[i] == b[i + 1])
+    {
+        c++;
+    }
+}
+Console.WriteLine(c);
