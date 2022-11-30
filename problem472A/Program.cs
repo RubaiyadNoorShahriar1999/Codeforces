b2005b9d03ccf23585e23ea9f48cabb224f630d9
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-11-30 22:52:30 +0600

    Problem 472A

---
+++ problem472A/Program.cs
@@ -0,0 +1,11 @@
+﻿int n;
+
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n % 2 == 0)
+    {
+        Console.WriteLine(n - 4 + " " +"4");
+    }
+    else
+    {
+        Console.WriteLine(n - 9 + " " +"9");
+    }
