d23e2d722c74204dc331ba9caec46785e63b02d2
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-02 03:30:13 +0600

    Problem 58A

---
+++ problem58A/Program.cs
@@ -0,0 +1,18 @@
+﻿string input, base_string = "hello";
+int j = 0, length = 0;
+
+input = Console.ReadLine();
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == base_string[j])
+        {
+            j++; length++;
+            if (length == 5)
+                break;
+        }
+    }
+    if (length == 5)
+        Console.WriteLine("YES");
+    else
+        Console.WriteLine("NO");
