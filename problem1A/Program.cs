18311fcdecd754ec6f4649e8b4bdffa97b4ad91e
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-02 03:28:05 +0600

    Problem 1A

---
+++ problem1A/Program.cs
@@ -0,0 +1,18 @@
+﻿long n, m, a, x, y, sum = 0;
+string[] data;
+
+data = Console.ReadLine().Split();
+n = Convert.ToInt32(data[0]);
+m = Convert.ToInt32(data[1]);
+a = Convert.ToInt32(data[2]);
+
+    x = n / a;
+    y = m / a;
+
+    if (n % a != 0)
+        x = x + 1;
+    if (m % a != 0)
+        y = y + 1;
+
+    sum = x * y;
+Console.WriteLine(sum);
