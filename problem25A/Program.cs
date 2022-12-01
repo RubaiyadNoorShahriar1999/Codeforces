fdf4401843ad4d4ef7ae265d0a30483a9e1e9958
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-02 03:28:57 +0600

    Problem 25A

---
+++ problem25A/Program.cs
@@ -0,0 +1,15 @@
+﻿int n, x, even =0, lastodd = 0, lasteven =0;
+string[] data;
+
+n = Convert.ToInt32(Console.ReadLine());
+data = Console.ReadLine().Split();
+
+for (int i = 0; i < n; i++)
+{
+    x = Convert.ToInt32(data[i]);
+    if (x % 2 == 0) { even += 1; lasteven = i+1; }
+    else { even -= 1; lastodd = i+1; }
+}
+
+int output =(even > 0 ? lastodd : lasteven);
+Console.WriteLine(output);
