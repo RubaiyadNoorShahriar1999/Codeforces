ff0c926fa4c3cffa99914f6e9cb7e5b05d64582d
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-02 03:29:24 +0600

    Problem 41A

---
+++ problem41A/Program.cs
@@ -0,0 +1,19 @@
+﻿int count = 0;
+string input1; 
+string input2;
+input1 = Console.ReadLine();
+input2 = Console.ReadLine();
+char[] a = input1.ToCharArray().Reverse().ToArray();
+
+for(int i = 0; i < a.Length; i++)
+{
+    if (a[i].Equals(input2[i]))
+        count++;
+    else
+        break;
+}
+
+if(count == input1.Length)
+    Console.WriteLine("YES");
+else
+    Console.WriteLine("NO");
