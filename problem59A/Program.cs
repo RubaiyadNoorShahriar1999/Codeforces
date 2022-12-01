4d16ff0842b6369cd1d017321e96adbbc928e39d
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-02 03:30:37 +0600

    Problem 59A

---
+++ problem59A/Program.cs
@@ -0,0 +1,35 @@
+﻿int countup = 0, countlow = 0;
+
+string s;
+s = Console.ReadLine();
+
+for (int i = 0; i < s.Length; i++)
+{
+    char c = s[i];
+    if (Char.IsUpper(c))
+    {
+        countup++;
+    }
+    else
+    {
+        countlow++;
+    }
+}
+if (countup > countlow)
+{
+    char ch;
+    for (int i = 0; i < s.Length; i++)
+    {
+        ch = char.ToUpper(s[i]);
+        Console.Write(ch);
+    }
+}
+else
+{
+    char ch;
+    for (int i = 0; i < s.Length; i++)
+    {
+        ch = char.ToLower(s[i]);
+        Console.Write(ch);
+    }
+}
