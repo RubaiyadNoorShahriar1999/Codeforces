009e0c4cf067292f51c9751b81eafe0f21be0f5a
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-03 00:06:36 +0600

    Problem 71A

---
+++ problem71A/Program.cs
@@ -0,0 +1,24 @@
+﻿int n;
+n = Convert.ToInt32(Console.ReadLine());
+
+List<string> word = new List<string>();
+
+for (int i = 0; i < n; i++)
+{
+    string x = Console.ReadLine();
+    word.Add(x);
+}
+
+for (int i = 0; i < word.Count; i++)
+{
+    string p = word[i];
+
+    if (p.Length > 10)
+    {
+        Console.WriteLine(p[0] + $"{p.Length - 2}" + p[p.Length - 1]);
+    }
+    else
+    {
+        Console.WriteLine(word[i]);
+    }
+}
