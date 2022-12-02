347e7020ef074be24ddbbbe1dcf124e9fa5adbdb
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-03 00:08:22 +0600

    Problem 122A

---
+++ problem122A/Program.cs
@@ -0,0 +1,7 @@
+﻿int a;
+a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 4 == 0 || a % 7 == 0 || a % 44 == 0 || a % 47 == 0 || a % 74 == 0 || a % 77 == 0 || a % 444 == 0 || a % 447 == 0 || a % 474 == 0 || a % 477 == 0 || a % 744 == 0 || a % 747 == 0 || a % 774 == 0 || a % 777 == 0)
+    Console.WriteLine("YES");
+else
+    Console.WriteLine("NO");
