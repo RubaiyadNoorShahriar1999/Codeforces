b2e58823e3cf3d1f1e484e4b55d6ab976ffe36dc
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-03 01:38:58 +0600

    Problem 155A

---
+++ problem155A/Program.cs
@@ -0,0 +1,25 @@
+﻿int n;
+n = Convert.ToInt32(Console.ReadLine());
+string[] data = Console.ReadLine().Split();
+int points = Convert.ToInt32(data[0]); 
+int min = points, max = points , amazing = 0;
+
+while (n != 0)
+{
+    for(int i = 0; i < data.Length; i++)
+    {
+        points = Convert.ToInt32(data[i]);
+    if (points < min)
+    {
+        min = points;
+        ++amazing;
+    }
+    if (points > max)
+    {
+        max = points;
+        ++amazing;
+    }
+    }
+    n--;
+}
+Console.WriteLine(amazing);
