4f9b971f9e007bef949c0c8ab5d6023925054602
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-03 00:08:51 +0600

    Problem 118A

---
+++ problem118A/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+string input;
+input = Console.ReadLine().ToLower();
+StringBuilder sb = new StringBuilder();
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u' || input[i] == 'y')
+        continue;
+    else
+    {
+        sb.Append('.');
+        sb.Append(input[i]);
+    }
+}
+Console.WriteLine(sb);
