1764d1cf018d49a636beefa7da36e6622d4b6863
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-03 00:09:38 +0600

    Problem 133A

---
+++ problem133A/Program.cs
@@ -0,0 +1,16 @@
+﻿string input;
+bool isTrue = false;
+input = Console.ReadLine();
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i] >= '!' && input[i] <= '~')
+    {
+        if (input[i] == 'H' || input[i] == 'Q' || input[i] == '9')
+            isTrue = true;
+    }
+}
+if(isTrue)
+    Console.WriteLine("YES");
+else
+    Console.WriteLine("NO");
