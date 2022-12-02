9a2bf0345e0ca53994c3f5990b097b5a032543a6
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-03 00:06:08 +0600

    Problem 61A

---
+++ problem61A/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+string s1, s2;
+s1 = Console.ReadLine();
+s2 = Console.ReadLine();
+
+StringBuilder sb = new StringBuilder(s1);
+
+for (int i = 0; i < s1.Length; ++i)
+{
+    if (s1[i] == s2[i])
+    {
+        sb[i] = '0';
+    }
+    else
+    {
+        sb[i] = '1';
+    }
+}
+Console.WriteLine(sb);
