2f5ead02e32a95d65c4b0557ec730684e7298a6c
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-03 00:10:23 +0600

    Problem 141A

---
+++ problem141A/Program.cs
@@ -0,0 +1,33 @@
+﻿string guest, residence_host, combined, pile;
+bool isTrue = false;
+
+guest = Console.ReadLine();
+residence_host = Console.ReadLine();
+pile = Console.ReadLine();
+combined = guest + residence_host;
+
+char[] x = combined.ToCharArray();
+Array.Sort(x);
+char[] y = pile.ToCharArray();
+Array.Sort(y);
+
+if(x.Length== y.Length)
+{
+    for (int i = 0; i < x.Length; i++)
+    {
+        if (x[i] == y[i])
+            isTrue = true;
+        else
+        {
+            isTrue = false;
+            break;
+        }
+    }
+}
+else
+    isTrue = false;
+
+if (isTrue)
+    Console.WriteLine("YES");
+else
+    Console.WriteLine("NO");
