6e98ed41187fa811570a034c7f725a4c2373d3fe
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-03 00:07:50 +0600

    Problem 110A

---
+++ problem110A/Program.cs
@@ -0,0 +1,17 @@
+﻿string input;
+int count = 0;
+
+input = Console.ReadLine();
+
+for(int i = 0; i < input.Length; i++)
+{
+    if (input[i] == '4' || input[i] == '7') 
+        count++;
+    else
+        continue;
+}
+
+if(count == 4 || count == 7)
+    Console.WriteLine("YES");
+else
+    Console.WriteLine("NO");
