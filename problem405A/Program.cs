0fa43d6836a2cbbe06c01daf0b785288c2babe2f
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-03 17:03:17 +0600

    Problem 405A

---
+++ problem405A/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+List<int> list = new List<int>();
+string[] data = Console.ReadLine().Split();
+
+for(int i = 0; i < n; i++)
+{
+    int x = Convert.ToInt32(data[i]);
+    list.Add(x);
+}
+
+list.Sort();
+for (int i = 0; i < n; i++)
+    Console.Write(list[i] + " ");
