51cae17976834c2f3cc87b1a046acc60e5f95716
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-03 19:20:54 +0600

    Problem 467A

---
+++ problem467A/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.Metrics;
+
+int n = Convert.ToInt32(Console.ReadLine());
+int counter = 0;
+for (int i = 0; i < n; i++)
+{
+    string[] data1 = new string[2];
+    data1 = Console.ReadLine().Split();
+    int[,] x = new int[n, 2];
+    for (int j = 0; j < 2; j++)
+    {
+        x[i, j] = Convert.ToInt32(data1[j]);
+    }
+    for (int k = 0; k < 1; k++)
+    {
+        if (x[i, k+1] - x[i, k] >= 2)
+        {
+            counter++;
+        }
+    }
+}
+Console.WriteLine(counter);
