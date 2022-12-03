7a3c02d65b15da003b73effa65b89ba123cdb3f7
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-03 16:34:59 +0600

    Problem 344A

---
+++ problem344A/Program.cs
@@ -0,0 +1,13 @@
+﻿int n,count = 0;
+n = Convert.ToInt32(Console.ReadLine());
+int[] a = new int[1000000];
+
+for (int i = 0; i < n; i++)
+    a[i] = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    if (a[i] != a[i + 1])
+        count++;
+}
+Console.WriteLine(count);
