9e79738ffc3512fd003942c8d27fcfb0dcf0a4c9
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-03 16:10:12 +0600

    Problem 228A

---
+++ problem228A/Program.cs
@@ -0,0 +1,17 @@
+﻿List<int> list = new List<int>(5);
+int min = 3;
+string[] data;
+data = Console.ReadLine().Split();
+
+    for (int i = 0; i < 4; i++)
+    {
+        int x = Convert.ToInt32(data[i]);
+        list.Add(x);     
+    }
+    list.Sort();
+    for (int i = 0; i < 3; i++)
+    {
+        if (list[i] != list[i + 1])
+            min--;
+    }
+Console.WriteLine(min);
