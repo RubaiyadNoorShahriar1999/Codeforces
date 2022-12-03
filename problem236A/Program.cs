13fb5f87eb71d8da18eb17e59af95c41818d24ea
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-03 15:56:24 +0600

    Problem 236A

---
+++ problem236A/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+int num = 0,pass = 1; 
+string input;
+input = Console.ReadLine();
+
+for (int i = 0; i < input.Length; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+        if (input[i] == input[j])
+        {
+            num++;
+        }
+    }
+    if (num == 0)
+    {
+        pass++;
+    }
+    num = 0;
+}
+
+if (pass % 2 == 0)
+    Console.WriteLine("IGNORE HIM!");
+else
+    Console.WriteLine("CHAT WITH HER!");
