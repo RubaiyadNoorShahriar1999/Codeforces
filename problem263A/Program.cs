45d9102c0aa47285a4cce3716769ff06298e5421
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-04 00:13:46 +0600

    Problem 263A

---
+++ problem263A/Program.cs
@@ -0,0 +1,17 @@
+﻿int _i = 0, _j = 0;
+
+for (int i = 1; i <= 5; i++)
+{
+    string[] input = Console.ReadLine().Split();
+    for (int k = 1; k <= 5; k++)
+    {
+        int data = Convert.ToInt32(input[k - 1]);
+        if (data == 1)
+        {
+            _i = i;
+            _j = k;
+        }
+    }
+}
+Console.WriteLine(Math.Abs(_i - 3) + Math.Abs(_j - 3));
+
