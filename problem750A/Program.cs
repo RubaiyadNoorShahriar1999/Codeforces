5e69b93ca84b8e41fd94094fbcdb9aa972bc1b8c
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-05 02:07:43 +0600

    Problem 750A

---
+++ problem750A/Program.cs
@@ -0,0 +1,15 @@
+﻿int n, k, count = 0,time;
+string[] data;
+data = Console.ReadLine().Split();
+n = Convert.ToInt32(data[0]);
+k= Convert.ToInt32(data[1]);
+time = 240 - k;
+for(int i = 1; i <= n;i++)
+{
+    if (time / (5 * i) > 0)
+    {
+        time = time - (5*i);
+        count++;
+    }
+}
+Console.WriteLine(count);
