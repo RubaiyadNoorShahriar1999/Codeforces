3a0631255989e2bdad85632116b0dcb646b484b0
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-05 03:16:35 +0600

    Problem 791A

---
+++ problem791A/Program.cs
@@ -0,0 +1,16 @@
+﻿int a, b, i;
+string[] data;
+data = Console.ReadLine().Split();
+
+a = Convert.ToInt32(data[0]);
+b = Convert.ToInt32(data[1]);
+
+for(i = 1; i<10; i++)
+{
+    a = a * 3;
+    b = b * 2;
+
+    if (a > b)
+        break;
+}
+Console.WriteLine(i);
