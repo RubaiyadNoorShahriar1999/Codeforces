e2c4672956059740e3bcde0f39d9282d398433d5
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-05 01:52:44 +0600

    Problem 732A

---
+++ problem732A/Program.cs
@@ -0,0 +1,15 @@
+﻿int k, r,count = 0;
+bool isTrue = true;
+string[] data;
+data = Console.ReadLine().Split();
+k = Convert.ToInt32(data[0]);
+r = Convert.ToInt32(data[1]);
+
+while (isTrue)
+{
+    count++;
+    if((k * count) % 10 == 0 || (k * count) % 10 == r)
+        isTrue= false;
+}
+Console.WriteLine(count);
+
