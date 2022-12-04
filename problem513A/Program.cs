b0a6d8a17ce31080059b29a947938cf2902713f5
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-04 23:04:42 +0600

    Problem 513A

---
+++ problem513A/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] data;
+data = Console.ReadLine().Split();
+
+int n1,n2,k1,k2;
+n1 = Convert.ToInt32(data[0]);
+n2 = Convert.ToInt32(data[1]);
+k1 = Convert.ToInt32(data[2]);
+k2 = Convert.ToInt32(data[3]);
+
+if(n1 <= n2)
+    Console.WriteLine("Second");
+else if(n1 > n2)
+    Console.WriteLine("First");
