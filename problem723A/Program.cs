26fc7f648c65a4c67748e1b4bec8e6c6b9dfc136
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-05 01:11:37 +0600

    Problem 723A

---
+++ problem723A/Program.cs
@@ -0,0 +1,11 @@
+﻿List<int> distance = new List<int>{};
+string[] data;
+data = Console.ReadLine().Split();
+
+for(int i = 0; i < data.Length; i++)
+{
+    int x = Convert.ToInt32(data[i]);
+    distance.Add(x);
+}
+distance.Sort();
+Console.WriteLine((distance[2] - distance[1]) + (distance[1] - distance[0]));
