b194ec46a36848735c5272ab3f4bd3f472b53648
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-04 23:13:00 +0600

    Problem 546A

---
+++ problem546A/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] data;
+data = Console.ReadLine().Split();
+int n, t, k, w;
+long sum = 0;
+k = Convert.ToInt32(data[0]);
+n = Convert.ToInt32(data[1]);
+w = Convert.ToInt32(data[2]);
+
+for (int i = 1; i <= w; i++)
+     sum = sum + (i * k);
+
+if (sum <= n)
+    Console.WriteLine(0);
+else 
+    Console.WriteLine(sum - n);
