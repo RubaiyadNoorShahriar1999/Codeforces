e7792ee6593e0374dcd0a5bf4d710026f0e17237
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-06 01:01:34 +0600

    Problem 282A

---
+++ problem282A/Program.cs
@@ -0,0 +1,22 @@
+﻿int n = Convert.ToInt32(Console.ReadLine()), x = 0;
+
+for (int i = 0; i < n; i++)
+{
+    string s;
+    s = Console.ReadLine();
+    for (int j = 0; j < s.Length; j += 2)
+    {
+        if (s[j] == '+')
+        {
+            x++;
+            break;
+        }
+        else if (s[j] == '-')
+        {
+            x--;
+            break;
+        }
+    }
+}
+
+Console.WriteLine(x);
