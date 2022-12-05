b373302d03e0cfd184c5d3811c0ef54ac1ea614a
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-06 02:01:54 +0600

    Problem 136A

---
+++ problem136A/Program.cs
@@ -0,0 +1,16 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int[] array= new int[200];
+string[] data;
+data = Console.ReadLine().Split();
+
+for (int i = 1;i <=n; i++)
+{
+    int x = Convert.ToInt32(data[i-1]);
+    array[x] = i;
+}
+
+for (int i = 1; i <= array.Length-1; i++)
+{
+    if (array[i] != 0)
+        Console.Write(array[i] + " ");
+}
