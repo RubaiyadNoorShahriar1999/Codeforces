0dfa531e5455193e3c0ca926d6202613f7d3098c
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-05 08:01:17 +0600

    Problem 1030A

---
+++ problem1030A/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = Convert.ToInt32(Console.ReadLine()),count =0;
+string[] data;
+data = Console.ReadLine().Split();
+
+for(int i = 0; i < n; i++)
+{
+    if (data[i] == "1")
+        count++;
+}
+if(count > 0)
+   Console.WriteLine("HARD");
+else
+    Console.WriteLine("EASY");
