9d78be81548cff21c852441d8a6c49944a7b1a35
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-07 19:52:02 +0600

    Problem 977A

---
+++ problem977A/Program.cs
@@ -0,0 +1,15 @@
+﻿int n, k, rem, i;
+string[] data = Console.ReadLine().Split();
+n = Convert.ToInt32(data[0]);
+k = Convert.ToInt32(data[1]);
+
+for (i = 1; i <= k; i++)
+{
+    rem = n % 10;
+
+    if (rem == 0)
+        n /= 10;
+    else
+        n -= 1;
+}
+Console.WriteLine(n);
