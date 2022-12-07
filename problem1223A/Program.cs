bbb0e773a017e0e9ce5d0918b3d7000c92ec648a
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-08 04:01:29 +0600

    Problem 1223A

---
+++ problem1223A/Program.cs
@@ -0,0 +1,19 @@
+﻿int m = 0, i;
+i = Convert.ToInt32(Console.ReadLine());
+
+int[] n = new int[i];
+
+for(int x = 0; x < i; x++)
+    n[x] = Convert.ToInt32(Console.ReadLine());
+
+
+for (int x = 0; x < i; x++)
+{
+    if (n[x] == 2)
+        Console.WriteLine(2);
+    else
+    {
+        m = n[x] % 2;
+        Console.WriteLine(m);
+    }
+}
