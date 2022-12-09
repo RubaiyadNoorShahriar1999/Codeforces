65feeb8644326fb13588ff290e8d4802a941d3f0
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-09 15:27:48 +0600

    Problem 1312A

---
+++ problem1312A/Program.cs
@@ -0,0 +1,19 @@
+﻿int t, m, n;
+t = Convert.ToInt32(Console.ReadLine());
+List<string> list = new List<string>();
+while (t != 0)
+{
+    string[] data = Console.ReadLine().Split();
+    n = Convert.ToInt32(data[0]);
+    m = Convert.ToInt32(data[1]);
+
+    if (n % m == 0)
+        list.Add("YES");
+    else
+        list.Add("NO");
+
+    t--;
+}
+
+foreach (string s in list)
+    Console.WriteLine(s);
