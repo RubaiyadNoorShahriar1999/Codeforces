6e2b094a73bb8cfccc4d88ace35f7e7c43503734
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-13 02:04:30 +0600

    Problem 1409A

---
+++ problem1409A/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+int t, a, b;
+List<int> list = new List<int>();
+t = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < t; i++)
+{
+    string[] data = Console.ReadLine().Split();
+    a = Convert.ToInt32(data[0]);
+    b = Convert.ToInt32(data[1]);
+
+    if (a == b)
+        list.Add(0);
+    else
+    {
+        int greater = (a > b ? a : b);
+        int less = (a > b ? b : a);
+        list.Add(Math.Abs(greater - less) / 10 + (Math.Abs(greater - less) % 10 > 0 ? 1 : 0));
+    }
+}
+foreach (int x in list)
+    Console.WriteLine(x);
