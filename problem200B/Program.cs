603a30eb45a154f270efd7d299566f16436009d8
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-13 00:54:56 +0600

    Problem 200B

---
+++ problem200B/Program.cs
@@ -0,0 +1,11 @@
+﻿List<int> list = new List<int>(110);
+int n = Convert.ToInt32(Console.ReadLine());
+string[] data = Console.ReadLine().Split();
+int total = 0;
+
+for (int i = 0; i < n; i++)
+{
+    int x = Convert.ToInt32(data[i]);
+    total += x;
+}
+Console.WriteLine(Convert.ToDouble(total) / n);
