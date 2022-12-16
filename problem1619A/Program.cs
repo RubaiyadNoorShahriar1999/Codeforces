97e31cd920e094fca3974b0845a9315f9ced464f
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-16 17:20:19 +0600

    Problem 1619A

---
+++ problem1619A/Program.cs
@@ -0,0 +1,35 @@
+﻿int t = Convert.ToInt32(Console.ReadLine());
+List<string> list = new List<string>();
+
+while (t != 0)
+{
+    string s;
+    s = Console.ReadLine();
+    bool isTrue = false;
+
+    if (s.Length % 2 != 0)
+        isTrue = false;
+    else
+    {
+        for (int i = 0; i < s.Length / 2;)
+        {
+            if (s[i] == s[i + s.Length / 2])
+            {
+                isTrue= true;
+                i++;
+            }
+            else
+            {
+                isTrue= false;
+                break;
+            }
+        }
+    }
+    if(isTrue)
+        list.Add("YES");
+    else
+        list.Add("NO");
+    t--;
+}
+foreach(string  s in list)
+{ Console.WriteLine(s); }
