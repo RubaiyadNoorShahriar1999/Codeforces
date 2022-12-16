7e755715d7ce97236425c49419dda91828cc3640
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-16 15:58:39 +0600

    Problem 1367A

---
+++ problem1367A/Program.cs
@@ -0,0 +1,19 @@
+﻿int t;
+t = Convert.ToInt32(Console.ReadLine());
+List<char> list = new List<char>();
+
+while (t != 0)
+{
+    string b;
+    b = Console.ReadLine();
+
+    list.Add(b[0]);
+    for (int i = 1; i < b.Length; i+=2)
+        list.Add(b[i]);
+
+    list.Add('\n');
+    t--;
+}
+
+foreach(char c in list)
+{ Console.Write(c); }
