377298f6d99c461dd2a4874a40a4add6771e9d36
Rubaiyad Noor Shahriar <rubaiyadnoorshahriar@gmail.com>
2022-12-17 15:55:44 +0600

    Problem 1754A

---
+++ problem1754A/Program.cs
@@ -0,0 +1,36 @@
+﻿int t;  
+t = Convert.ToInt32(Console.ReadLine());
+List<string> list = new List<string>();
+
+while (t != 0)
+{
+    bool isTrue = false;
+    int counta = 0, countq = 0;
+    int n = Convert.ToInt32(Console.ReadLine());
+    string s = Console.ReadLine();
+
+    if (s[s.Length - 1] != 'A')
+        list.Add("NO");
+    else
+    {
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            if (s[i] == 'Q')
+                countq++;
+            else
+                counta++;
+            if (countq > counta)
+            {
+                list.Add("NO");
+                isTrue = true;
+                break;
+            }
+        }
+        if (!isTrue)
+            list.Add("YES");
+    }
+    t--;
+}
+
+foreach(string s in list)
+    Console.WriteLine(s);
